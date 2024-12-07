ca2b1a23f374a542f0166493453897750f7e7439
hibaallahinfo <hibaallahessarghi@gmail.com>
2024-12-07 18:49:24 +0100

    commit

---
+++ _temp/dbfb7889-cffe-486a-b095-fc1b12916618.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'stop'
+$ProgressPreference = 'silentlyContinue'
+$ErrorView = 'NormalView'
+curl.exe -X POST -H "Authorization: Bearer $env:RENDER_API_KEY" `
+-H "Content-Type: application/json" `
+-d '{\"serviceId\":\"docker-image\",\"clearCache\":true}' `
+"https://api.render.com/v1/services/docker-image/deploy"
+if (!(Test-Path -LiteralPath variable:\LASTEXITCODE)) {
+    Write-Host '##vso[task.debug]$LASTEXITCODE is not set.'
+} else {
+    Write-Host ('##vso[task.debug]$LASTEXITCODE: {0}' -f $LASTEXITCODE)
+    exit $LASTEXITCODE
+}
